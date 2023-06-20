9059608213258465f10014cfd98b72a03d1670c5
vikrant <vikrant1k978@gmail.com>
2023-06-20 14:22:41 +0530

    post creation

--- src/component/Home/share.js
+++ src/component/Home/share.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {
   Avatar,
   Button,
@@ -13,7 +14,6 @@ import {
 import React, { useState } from "react";
 import {
   Add as AddIcon,
-  
   EmojiEmotions,
   Image,
   PersonAdd,
@@ -35,7 +35,36 @@ const UserBox = styled(Box)({
 });
 
 const Add = () => {
+  const BASE_URL = process.env.REACT_APP_BASE_URL;
   const [open, setOpen] = useState(false);
+  const [post, setPost] = useState({});
+  const Id = localStorage.getItem("userId");
+
+  const val = (e) => {
+    e.preventDefault();
+    const { value, name } = e.target;
+    setPost(() => {
+      return {
+        ...post,
+        [name]: value,
+        "userId": Id,
+      };
+    });
+  };
+
+  const handlClick = (e) => {
+    e.preventDefault();
+    const { userId, title, caption } = post;
+    if (userId && title && caption) {
+      axios.post(`${BASE_URL}/post/newpost`, post).then((res) => {
+        alert(res.data.message);
+      });
+    } else {
+      alert("Post needs both a title and a caption");
+    }
+    setOpen(false);
+  };
+
   return (
     <>
       <Tooltip
@@ -53,26 +82,30 @@ const Add = () => {
       </Tooltip>
       <SytledModal
         open={open}
-        onClose={(e) => setOpen(false)}
+        onClose={(e) => {
+          setOpen(false);
+          setPost({});
+        }}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <Box
           width={400}
           height={280}
-          bgcolor={"background.default"} 
+          bgcolor={"background.default"}
           color={"text.primary"}
           p={3}
           borderRadius={5}
         >
-          <Typography variant="h6" color="backgorund.default" textAlign="center">
+          <Typography
+            variant="h6"
+            color="backgorund.default"
+            textAlign="center"
+          >
             Create post
           </Typography>
           <UserBox>
-            <Avatar
-              src=""
-              sx={{ width: 30, height: 30 }}
-            />
+            <Avatar src="" sx={{ width: 30, height: 30 }} />
             <Typography fontWeight={500} variant="span">
               Kunal Chaurasia
             </Typography>
@@ -80,10 +113,21 @@ const Add = () => {
           <TextField
             sx={{ width: "100%" }}
             id="standard-multiline-static"
+            name="title"
+            rows={3}
+            placeholder="Title"
+            variant="standard"
+            onChange={val}
+          />
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
             multiline
+            name="caption"
             rows={3}
             placeholder="What's on your mind?"
             variant="standard"
+            onChange={val}
           />
           <Stack direction="row" gap={1} mt={2} mb={3}>
             <EmojiEmotions color="primary" />
@@ -95,6 +139,7 @@ const Add = () => {
             fullWidth
             variant="contained"
             aria-label="outlined primary button group"
+            onClick={handlClick}
           >
             <Button>Post</Button>
           </ButtonGroup>

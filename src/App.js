774cf266526db2a07d41fa3177f9b47a45593aef
Asif <rahmanasif158@gmail.com>
2023-07-26 01:29:42 +0530

    Added Custom Global Scrollbars Support

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useState } from "react";
+import "./Global.css"
 import "./Pages/LoginForm";
 import LoginForm from "./Pages/LoginForm";
 import ForgotPass from "./Pages/ForgotPass";
 import Homepage from "./Pages/homepage";
 import Profile from "./Pages/Profile";
 import ChangePass from "./Pages/ChangePass";
+
 
 function App() {
   //UserLogin
@@ -18,34 +20,35 @@ function App() {
   console.log(user._id);
 
   return (
-    <div className="App" >
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              (user && user._id) || loggedIn ? (
-                <Homepage setLoginUser={setLoginUser} />
-              ) : (
-                <LoginForm setLoginUser={setLoginUser} />
-              )
-            }
-          />
-          <Route
-            path="/LoginForm"
-            element={<LoginForm setLoginUser={setLoginUser} />}
-          />
-          <Route path="/Forgotpassword" element={<ForgotPass />} />
-          <Route path="/Changepassword" element={<ChangePass />} />
+      <div className="App" >
+        <BrowserRouter>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                (user && user._id) || loggedIn ? (
+                  <Homepage setLoginUser={setLoginUser} />
+                ) : (
+                  <LoginForm setLoginUser={setLoginUser} />
+                )
+              }
+            />
+            <Route
+              path="/LoginForm"
+              element={<LoginForm setLoginUser={setLoginUser} />}
+            />
+            <Route path="/Forgotpassword" element={<ForgotPass />} />
+            <Route path="/Changepassword" element={<ChangePass />} />
 
-          <Route
-            path="/Profile"
-            element={<Profile setLoginUser={setLoginUser} />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
+            <Route
+              path="/Profile"
+              element={<Profile setLoginUser={setLoginUser} />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </div>
+
   )
 }
 

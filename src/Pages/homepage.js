9ce22d10deb3525ceee87f1d42a3571cf6c543de
vikrant <vikrant1k978@gmail.com>
2023-08-26 17:06:48 +0530

    Feat: Create Question

--- src/Pages/homepage.js
+++ src/Pages/homepage.js
@@ -7,6 +7,7 @@ import Header from "../component/Home/Header";
 import Share from "../component/Home/share";
 
 const Homepage = ({ setLoginUser }) => {
+	const [create,setCreate]=useState(false)
 	//Theme
 	const [mode, setMode] = useState("light");
 
@@ -27,38 +28,48 @@ const Homepage = ({ setLoginUser }) => {
 	});
 
 	return (
-		<ThemeProvider theme={darkTheme}>
-			<div className="homepage">
-				<Box bgcolor={"background.default"} color={"text.primary"} height={"100%"}>
-					<Header setLoginUser={setLoginUser} setMode={setMode} mode={mode} />
-					<div style={{ display: "flex" }}>
-						<Box sx={{ flex: { md: "2", lg: "3" }, display: { xs: "none", md: "block" } }}>
-							<Recents />
-						</Box>
-						<Box
-							sx={{
-								flex: {
-									md: "4",
-									lg: "6",
-								},
-							}}
-						>
-							<Share />
-							<FeedSwitch />
-						</Box>
-						<Box
-							sx={{
-								flex: 3,
-								display: { xs: "none", big: "block" },
-							}}
-						>
-							<LeftBar />
-						</Box>
-					</div>
-				</Box>
-			</div>
-		</ThemeProvider>
-	);
+    <ThemeProvider theme={darkTheme}>
+      <div className="homepage">
+        <Box
+          bgcolor={"background.default"}
+          color={"text.primary"}
+          height={"100%"}
+        >
+          <Header setLoginUser={setLoginUser} setMode={setMode} mode={mode} />
+          <div style={{ display: "flex" }}>
+            <Box
+              sx={{
+                flex: { md: "2", lg: "3" },
+                display: { xs: "none", md: "block" },
+              }}
+            >
+              <Recents />
+            </Box>
+            <Box
+              sx={{
+                flex: {
+                  md: "4",
+                  lg: "6",
+                },
+              }}
+            >
+              <Share setCreate={setCreate} create={create} />
+              <FeedSwitch setCreate={setCreate} create={create} />
+              
+            </Box>
+            <Box
+              sx={{
+                flex: 3,
+                display: { xs: "none", big: "block" },
+              }}
+            >
+              <LeftBar />
+            </Box>
+          </div>
+        </Box>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default Homepage;
--- src/component/Home/share.js
+++ src/component/Home/share.js
@@ -10,6 +10,7 @@ import {
   TextField,
   Tooltip,
   Typography,
+  Container,
 } from "@mui/material";
 import React, { useState } from "react";
 import {
@@ -20,6 +21,11 @@ import {
   VideoCameraBack,
 } from "@mui/icons-material";
 import { Box } from "@mui/system";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import { Paper } from "@mui/material";
 
 const SytledModal = styled(Modal)({
   display: "flex",
@@ -34,11 +40,41 @@ const UserBox = styled(Box)({
   marginBottom: "20px",
 });
 
-const Add = () => {
+const Add = (props) => {
   const BASE_URL = process.env.REACT_APP_BASE_URL;
   const [open, setOpen] = useState(false);
   const [post, setPost] = useState({});
+  const [value, setValue] = useState("0");
   const Id = localStorage.getItem("userId");
+    
+
+  //image upload
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    // const formData = new FormData();
+    // formData.append("image", file);
+
+    reader.onloadend = () => {
+      setImageUrl(reader.result);
+          console.log(value);
+
+    };
+    if (file) {
+      reader.readAsDataURL(file);
+    }
+  };
+  const remove = () => {
+    setImageUrl(null);
+  };
+  //image upload
+
+  
 
   const val = (e) => {
     e.preventDefault();
@@ -47,20 +83,45 @@ const Add = () => {
       return {
         ...post,
         [name]: value,
-        "userId": Id,
+        userId: Id,
       };
     });
   };
-
   const handlClick = (e) => {
+    if(value == "0"){
+      handlClickPost(e);
+    }else{
+      handlClickQuestion(e);
+    }
+  }
+
+  console.log("POST",post);
+
+  const handlClickPost = (e) => {
     e.preventDefault();
     const { userId, title, caption } = post;
     if (userId && title && caption) {
       axios.post(`${BASE_URL}/post/newpost`, post).then((res) => {
         alert(res.data.message);
+        props.setCreate(!(props.create))
       });
     } else {
       alert("Post needs both a title and a caption");
+    }
+    setOpen(false);
+  };
+
+
+  const handlClickQuestion = (e) => {
+    e.preventDefault();
+    const { userId, title, description } = post;
+    if (userId && title && description) {
+      axios.post(`${BASE_URL}/question/newquestion`, post).then((res) => {
+        alert(res.data.message);
+        props.setCreate(!props.create);
+      });
+    } else {
+      alert("Question needs both a title and a description");
     }
     setOpen(false);
   };
@@ -85,33 +146,38 @@ const Add = () => {
         onClose={(e) => {
           setOpen(false);
           setPost({});
+          setImageUrl(null);
         }}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <Box
           width={400}
-          height={280}
+          // height={330}
           bgcolor={"background.default"}
           color={"text.primary"}
           p={3}
           borderRadius={5}
+          sx={{ display: "flex", flexDirection: "column", gap: 3 }}
         >
-          <Typography
-            variant="h6"
-            color="backgorund.default"
-            textAlign="center"
-          >
-            Create post
-          </Typography>
-          <UserBox>
-            <Avatar src="" sx={{ width: 30, height: 30 }} />
-            <Typography fontWeight={500} variant="span">
-              Kunal Chaurasia
-            </Typography>
-          </UserBox>
+          <Paper elevation={0}>
+            <Box sx={{ width: "100%", typography: "body1" }}>
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                  <TabList
+                    onChange={handleChange}
+                    aria-label="lab API tabs example"
+                  >
+                    <Tab label="Create Post" value="0" />
+                    <Tab label="Ask a Question" value="1" />
+                  </TabList>
+                </Box>
+              </TabContext>
+            </Box>
+          </Paper>
+
           <TextField
-            sx={{ width: "100%" }}
+            sx={{ width: "100%" }} //, marginBottom: "1rem" }}
             id="standard-multiline-static"
             name="title"
             rows={3}
@@ -123,19 +189,58 @@ const Add = () => {
             sx={{ width: "100%" }}
             id="standard-multiline-static"
             multiline
-            name="caption"
+            name={value==="0"?"caption":"description"}
             rows={3}
             placeholder="What's on your mind?"
             variant="standard"
             onChange={val}
           />
-          <Stack direction="row" gap={1} mt={2} mb={3}>
-            <EmojiEmotions color="primary" />
-            <Image color="secondary" />
-            <VideoCameraBack color="success" />
-            <PersonAdd color="error" />
-          </Stack>
+
+          <label
+            htmlFor="upload-image"
+            style={{ backgroundColor: "lightyellow" }}
+          >
+            <Button
+              variant="contained"
+              component="span"
+              sx={{
+                backgroundColor: "transparent",
+                "&:hover": { backgroundColor: "transparent" },
+                // display: "inline"
+                color: "black",
+                textTransform: "none",
+                width: "100%",
+              }}
+            >
+              Upload Image{" "}
+              <Image color="secondary" sx={{ marginLeft: "8px" }} />
+            </Button>
+            <input
+              id="upload-image"
+              hidden
+              accept="image/*"
+              type="file"
+              onChange={handleFileUpload}
+            />
+          </label>
+
+          {imageUrl && (
+            <>
+              <Box
+                width="auto"
+                height={100}
+                sx={{
+                  backgroundImage: `url(${imageUrl})`,
+                  backgroundPosition: "center",
+                  backgroundSize: "contain",
+                  backgroundRepeat: "no-repeat",
+                }}
+              ></Box>
+              <Button onClick={() => remove()}>remove image</Button>
+            </>
+          )}
           <ButtonGroup
+            // sx={{ marginTop: "5px" }}
             fullWidth
             variant="contained"
             aria-label="outlined primary button group"

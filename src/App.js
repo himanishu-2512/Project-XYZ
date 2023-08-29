16183bb42239eadd3e6e82e7721d4086573266bf
Prakhar Srivastav <prak13580@gmail.com>
2023-08-30 00:23:26 +0530

    Fix: View post page routing

--- src/App.js
+++ src/App.js
@@ -7,53 +7,54 @@ import ForgotPass from "./Pages/ForgotPass";
 import Homepage from "./Pages/homepage";
 import Profile from "./Pages/Profile";
 import ChangePass from "./Pages/ChangePass";
+import ViewPost from "./Pages/ViewPost";
+import ViewQuestion from "./Pages/ViewQuestion";
 
 function App() {
-  //UserLogin
-  const loggedIn = window.localStorage.getItem("isLoggedIn");
-  const [user, setLoginUser] = useState({
-    rNum: "",
-    dob: "",
-    pass: "",
-  });
-  // console.log(user._id);
+	//UserLogin
+	const loggedIn = window.localStorage.getItem("isLoggedIn");
+	const [user, setLoginUser] = useState({
+		rNum: "",
+		dob: "",
+		pass: "",
+	});
+	// console.log(user._id);
 
-  return (
-    <div className="App" style={{ backgroundColor:"#0F172A"}}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              (user && user._id) || loggedIn ? (
-                <Homepage setLoginUser={setLoginUser} />
-              ) : (
-                <LoginForm setLoginUser={setLoginUser} />
-              )
-            }
-          />
-          <Route
-            path="/LoginForm"
-            element={<LoginForm setLoginUser={setLoginUser} />}
-          />
-          <Route path="/Forgotpassword" element={<ForgotPass />} />
-          <Route path="/Changepassword" element={<ChangePass />} />
+	return (
+		<div className="App" style={{ backgroundColor: "#0F172A" }}>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={
+							(user && user._id) || loggedIn ? (
+								<Homepage setLoginUser={setLoginUser} />
+							) : (
+								<LoginForm setLoginUser={setLoginUser} />
+							)
+						}
+					/>
+					<Route path="/LoginForm" element={<LoginForm setLoginUser={setLoginUser} />} />
+					<Route path="/Forgotpassword" element={<ForgotPass />} />
+					<Route path="/Changepassword" element={<ChangePass />} />
 
-          <Route
-            path="/Profile/:username"
-            element={
-              (user && user._id) || loggedIn ? (
-                <Profile setLoginUser={setLoginUser} />
-              ) : (
-                <LoginForm setLoginUser={setLoginUser} />
-              )
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+					<Route
+						path="/Profile/:username"
+						element={
+							(user && user._id) || loggedIn ? (
+								<Profile setLoginUser={setLoginUser} />
+							) : (
+								<LoginForm setLoginUser={setLoginUser} />
+							)
+						}
+					/>
+					<Route path="/post/:postId" element={<ViewPost />} />
+					<Route path="/question/:questionId" element={<ViewQuestion />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
